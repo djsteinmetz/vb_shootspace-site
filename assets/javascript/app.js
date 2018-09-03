23af72417ef77cd82b1cc74621b3e34a6796b46d
Ansel Adams <dj@volaciousbrands.com>
2018-09-02 23:17:17 -0500

    ux styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
         // Display starts off on SEMI-ANNUAL PRICING
         // console.log("Current billing ID: " + this.id);
         switch (this.id) {
-            // Basic Pricing
             case "switchToAnnual":
                 $("#basicSemiAnnualPrice").html("<h2>$20/month</h2><h6>Annual</br>Billed every year - save 30%</h6>")
                     .addClass("card-text")
@@ -19,6 +18,7 @@ $(document).ready(function () {
                 $("#switchToAnnual").html('> Switch to Monthly Billing')
                     .addClass("switchBilling")
                     .attr("id", "switchToMonthly");
+                // BASIC ANNUAL PRICING LINK GOES BELOW BETWEEN QUOTES, REPLACE ##
                 $("#basicBtn").attr("href", "##");
                 break;
 
@@ -29,19 +29,20 @@ $(document).ready(function () {
                 $("#switchToMonthly").html('> Switch to Semi-Annual Billing')
                     .addClass("switchBilling")
                     .attr("id", "switchToSemiAnnual");
+                // BASIC MONTHLY PRICING LINK GOES BELOW BETWEEN QUOTES, REPLACE ###
                 $("#basicBtn").attr("href", "###");
                 break;
-            
+
             case "switchToSemiAnnual":
-            $("#basicMonthlyPrice").html("<h2>$23/month</h2><h6>Semi-Annual</br>Billed every 6mo - save 20%</h6>")
-                .addClass("card-text")
-                .attr("id", "basicSemiAnnualPrice");
-            $("#switchToSemiAnnual").html('> Switch to Annual Billing')
-                .addClass("switchBilling")
-                .attr("id", "switchToAnnual");
-            $("#basicBtn").attr("href", "#");
-            break;
-            // Premium Pricing
+                $("#basicMonthlyPrice").html("<h2>$23/month</h2><h6>Semi-Annual</br>Billed every 6mo - save 20%</h6>")
+                    .addClass("card-text")
+                    .attr("id", "basicSemiAnnualPrice");
+                $("#switchToSemiAnnual").html('> Switch to Annual Billing')
+                    .addClass("switchBilling")
+                    .attr("id", "switchToAnnual");
+                // BASIC SEMI ANNUAL PRICING LINK GOES BELOW BETWEEN QUOTES, REPLACE #
+                $("#basicBtn").attr("href", "#");
+                break;
             case "switchToAnnualPremium":
                 $("#premiumSemiAnnualPrice").html("<h2>$139/month</h2><h6>Annual</br>Billed every year - save 30%</h6>")
                     .addClass("card-text")
@@ -49,6 +50,7 @@ $(document).ready(function () {
                 $("#switchToAnnualPremium").html('> Switch to Monthly Billing')
                     .addClass("switchBilling")
                     .attr("id", "switchToMonthlyPremium");
+                // PREMIUM ANNUAL PRICING LINK GOES BELOW BETWEEN QUOTES, REPLACE #####
                 $("#premiumBtn").attr("href", "#####");
                 break;
 
@@ -59,29 +61,26 @@ $(document).ready(function () {
                 $("#switchToMonthlyPremium").html('> Switch to Semi-Annual Billing')
                     .addClass("switchBilling")
                     .attr("id", "switchToSemiAnnualPremium");
+                // PREMIUM MONTHLY PRICING LINK GOES BELOW BETWEEN QUOTES, REPLACE ######
                 $("#premiumBtn").attr("href", "######");
                 break;
-            
+
             case "switchToSemiAnnualPremium":
-            $("#premiumMonthlyPrice").html("<h2>$159/month</h2><h6>Semi-Annual</br>Billed every 6mo - save 20%</h6>")
-                .addClass("card-text")
-                .attr("id", "premiumSemiAnnualPrice");
-            $("#switchToSemiAnnualPremium").html('> Switch to Annual Billing')
-                .addClass("switchBilling")
-                .attr("id", "switchToAnnualPremium");
-            $("#premiumBtn").attr("href", "####");
+                $("#premiumMonthlyPrice").html("<h2>$159/month</h2><h6>Semi-Annual</br>Billed every 6mo - save 20%</h6>")
+                    .addClass("card-text")
+                    .attr("id", "premiumSemiAnnualPrice");
+                $("#switchToSemiAnnualPremium").html('> Switch to Annual Billing')
+                    .addClass("switchBilling")
+                    .attr("id", "switchToAnnualPremium");
+                // PREMIUM SEMI ANNUAL PRICING LINK GOES BELOW BETWEEN QUOTES, REPLACE ####
+                $("#premiumBtn").attr("href", "####");
                 break;
         };
     });
-});
-
-// Hide navbar on scroll down
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-$(document).ready(function() {
-    // Navbar banner animation
+    // Hide NavBar on scroll animation
     var banner_height = $("#navscroll").height();
     var lastScrollTop = 0;
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         var currScrollTop = $(this).scrollTop();
         if (scroll >= banner_height && currScrollTop > lastScrollTop) {
@@ -90,6 +89,11 @@ $(document).ready(function() {
             $("#banner").show(100);
         }
         lastScrollTop = currScrollTop;
+    });
+    // Toggle Collapse
+    $('.faq li .question').click(function () {
+        $(this).find('.plus-minus-toggle').toggleClass('collapsed');
+        $(this).parent().toggleClass('active');
     });
 });
 
--- assets/sharetribe/sharetribe.js
+++ assets/sharetribe/sharetribe.js
@@ -1,7 +1,59 @@
 $( document ).ready(function() {
-    var URL = $('[data-reactid="49"]');
-    var secondURL = $('[data-reactid="48"]');
-    URL[0].attributes.href.nodeValue = "http://www.shootspace.io";
-    URL[0].attributes.href.value = "http://www.shootspace.io";
-    secondURL[0].attributes[1].nodeValue = "http://www.shootspace.io";
+    if($('[data-reactid="33"]')) {
+        $('[data-reactid="33"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="34"]')) {
+        $('[data-reactid="34"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="35"]')) {
+        $('[data-reactid="35"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="36"]')) {
+        $('[data-reactid="36"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="37"]')) {
+        $('[data-reactid="37"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="38"]')) {
+        $('[data-reactid="38"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="39"]')) {
+        $('[data-reactid="39"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="40"]')) {
+        $('[data-reactid="40"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="41"]')) {
+        $('[data-reactid="41"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="42"]')) {
+        $('[data-reactid="42"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="43"]')) {
+        $('[data-reactid="43"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="44"]')) {
+        $('[data-reactid="44"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="45"]')) {
+        $('[data-reactid="45"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="46"]')) {
+        $('[data-reactid="46"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="47"]')) {
+        $('[data-reactid="47"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="48"]')) {
+        $('[data-reactid="48"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="49"]')) {
+        $('[data-reactid="49"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="50"]')) {
+        $('[data-reactid="50"]')[0].href = "https://www.shootspace.io";
+    }
+    if($('[data-reactid="51"]')) {
+        $('[data-reactid="51"]')[0].href = "https://www.shootspace.io";
+    }
 });
